72dcd0c62f10d2464e82f91a5de9d1c812764eb0
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-06-26 15:21:13 -0500

    Query APODs.

--- iOS/ApodTableViewController.cs
+++ iOS/ApodTableViewController.cs
@@ -1,23 +1,46 @@
 using Foundation;
 using System;
 using UIKit;
+using System.Collections.Generic;
+using HuePod.Nasa;
+using System.Threading.Tasks;
 
 namespace HuePod.iOS
 {
     public partial class ApodTableViewController : UITableViewController
     {
+
+		private List<Apod> _apods;
+		private Service _service;
+
         public ApodTableViewController (IntPtr handle) : base (handle)
         {
+			_apods = new List<Apod>();
         }
+
+		public override async void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			_service = new Service();
+			await LoadLastPictures();
+		}
 
 		public override nint NumberOfSections(UITableView tableView)
 		{
 			return 1;
 		}
 
+
+
+		private async Task LoadLastPictures()
+		{
+			_apods = await _service.GetLastAstronomicPictures(10);
+			base.TableView.ReloadData();
+		}
+
 		public override nint RowsInSection(UITableView tableView, nint section)
 		{
-			return 10;
+			return _apods.Count;
 		}
 
 		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
@@ -25,7 +48,9 @@ namespace HuePod.iOS
 			var cell = tableView.DequeueReusableCell("ApodTableViewCell", indexPath);
 			var apodCell = cell as ApodTableViewCell;
 
-			apodCell.ApodTitleLabel.Text = "APOD " + indexPath.Row;
+			var apod = _apods[indexPath.Row];
+
+			apodCell.ApodTitleLabel.Text = apod.Title;
 
 			return apodCell;
 		}
