91d799b7dc756fda553cf4f6fa7b4bca4e5eacfc
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-06-26 15:56:09 -0500

    iOS build errors fixd.

--- iOS/ApodTableViewCell.designer.cs
+++ iOS/ApodTableViewCell.designer.cs
@@ -11,19 +11,19 @@ using UIKit;
 
 namespace HuePod.iOS
 {
-    [Register ("ApodTableViewCell")]
-    partial class ApodTableViewCell
-    {
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UILabel apodTitleLabel { get; set; }
+	[Register ("ApodTableViewCell")]
+	partial class ApodTableViewCell
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel apodTitleLabel { get; set; }
 
-        void ReleaseDesignerOutlets ()
-        {
-            if (apodTitleLabel != null) {
-                apodTitleLabel.Dispose ();
-                apodTitleLabel = null;
-            }
-        }
-    }
+		void ReleaseDesignerOutlets ()
+		{
+			if (apodTitleLabel != null) {
+				apodTitleLabel.Dispose ();
+				apodTitleLabel = null;
+			}
+		}
+	}
 }
