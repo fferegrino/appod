b5458f74fe5edb0923879097bb1eb74c49985773
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-06-27 19:13:20 -0500

    Updates

--- iOS/ApodTableViewCell.designer.cs
+++ iOS/ApodTableViewCell.designer.cs
@@ -11,37 +11,35 @@ using UIKit;
 
 namespace HuePod.iOS
 {
-    [Register ("ApodTableViewCell")]
-    partial class ApodTableViewCell
-    {
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UILabel apodDateLabel { get; set; }
+	[Register ("ApodTableViewCell")]
+	partial class ApodTableViewCell
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel apodDateLabel { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIImageView apodImageView { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView apodImageView { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UILabel apodTitleLabel { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel apodTitleLabel { get; set; }
 
-        void ReleaseDesignerOutlets ()
-        {
-            if (apodDateLabel != null) {
-                apodDateLabel.Dispose ();
-                apodDateLabel = null;
-            }
-
-            if (apodImageView != null) {
-                apodImageView.Dispose ();
-                apodImageView = null;
-            }
-
-            if (apodTitleLabel != null) {
-                apodTitleLabel.Dispose ();
-                apodTitleLabel = null;
-            }
-        }
-    }
+		void ReleaseDesignerOutlets ()
+		{
+			if (apodDateLabel != null) {
+				apodDateLabel.Dispose ();
+				apodDateLabel = null;
+			}
+			if (apodImageView != null) {
+				apodImageView.Dispose ();
+				apodImageView = null;
+			}
+			if (apodTitleLabel != null) {
+				apodTitleLabel.Dispose ();
+				apodTitleLabel = null;
+			}
+		}
+	}
 }
--- iOS/ApodTableViewController.cs
+++ iOS/ApodTableViewController.cs
@@ -2,57 +2,94 @@ using Foundation;
 using System;
 using UIKit;
 using System.Collections.Generic;
+using System.Net.Http;
 using HuePod.Nasa;
 using System.Threading.Tasks;
+using AFNetworking;
 
 namespace HuePod.iOS
 {
     public partial class ApodTableViewController : UITableViewController
     {
 
-		private List<Apod> _apods;
-		private Service _service;
+        private List<Apod> _apods;
+        private Service _service;
 
-        public ApodTableViewController (IntPtr handle) : base (handle)
+        public ApodTableViewController(IntPtr handle) : base(handle)
         {
-			_apods = new List<Apod>();
+            _apods = new List<Apod>();
         }
 
-		public override async void ViewDidLoad()
-		{
-			base.ViewDidLoad();
-			_service = new Service();
-			await LoadLastPictures();
-		}
+        public override async void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            _service = new Service();
+            await LoadLastPictures();
+        }
 
-		public override nint NumberOfSections(UITableView tableView)
-		{
-			return 1;
-		}
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return 1;
+        }
 
 
 
-		private async Task LoadLastPictures()
-		{
-			_apods = await _service.GetLastAstronomicPictures(10);
-			base.TableView.ReloadData();
-		}
+        private async Task LoadLastPictures()
+        {
+            _apods = await _service.GetLastAstronomicPictures(10);
+            base.TableView.ReloadData();
+        }
 
-		public override nint RowsInSection(UITableView tableView, nint section)
-		{
-			return _apods.Count;
-		}
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return _apods.Count;
+        }
 
-		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
-		{
-			var cell = tableView.DequeueReusableCell("ApodTableViewCell", indexPath);
-			var apodCell = cell as ApodTableViewCell;
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("ApodTableViewCell", indexPath);
+            var apodCell = cell as ApodTableViewCell;
 
-			var apod = _apods[indexPath.Row];
+            var apod = _apods[indexPath.Row];
 
-			apodCell.ApodTitleLabel.Text = apod.Title;
+            apodCell.ApodTitleLabel.Text = apod.Title;
+            apodCell.ApodDateLabel.Text = $"{apod.Date:yyyy MMM dd}";
 
-			return apodCell;
-		}
+            //var imageTask = LoadImageFromURL(apod.Url);
+            //imageTask.Wait();
+            //apodCell.ImageView.Image = imageTask.Result;
+            if (apod.MediaType == "image")
+            {
+                apodCell.ImageView.SetImageUrl(new NSUrl(apod.Url), UIImage.FromFile("achede.jpg"));
+            }
+
+
+
+            //[imageView setImageWithURLRequest: request placeholderImage: nil success:^ (NSURLRequest * request, NSHTTPURLResponse * response, UIImage * image) {
+            //blockImageView.image = image; } failure: nil];
+
+            return apodCell;
+        }
+
+
+        /// <summary>
+        /// Loads the image.
+        /// </summary>
+        /// <returns>The image.</returns>
+        /// <param name="imageUrl">Image URL.</param>
+        public static async Task<UIImage> LoadImageFromURL(string imageUrl)
+        {
+            var httpClient = new HttpClient();
+
+            Task<byte[]> contentsTask = httpClient.GetByteArrayAsync(imageUrl);
+
+            // await! control returns to the caller and the task continues to run on another thread
+            var contents = await contentsTask;
+
+            System.Diagnostics.Debug.WriteLine(imageUrl);
+
+            // load from bytes
+            return UIImage.LoadFromData(NSData.FromArray(contents));
+        }
     }
 }
